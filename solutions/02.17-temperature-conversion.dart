3f2c2fa0958fc03d0fbb6b5d10adecc7708bf4da
Laura <law7676@outlook.com>
2021-06-05 17:13:59 -0500

    added temperature conversion solution

---
+++ solutions/02.17-temperature-conversion.dart
@@ -0,0 +1,7 @@
+void main() {
+  double tempFarenheit = 86;
+  print(tempFarenheit);
+  double tempCelsius = (tempFarenheit - 32) / 1.8;
+  print(tempCelsius);
+  print('${tempFarenheit}F = ${tempCelsius}C');
+}
