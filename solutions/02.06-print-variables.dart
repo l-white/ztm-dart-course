a33eecef3837752c1b90129f0c61d75458cc1045
Laura <law7676@outlook.com>
2021-06-05 10:42:47 -0500

    added print-variables solution

---
+++ solutions/02.06-print-variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  String firstName = 'Laura';
+  String lastName = 'White';
+  int age = 34;
+  double height = 5.25;
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
