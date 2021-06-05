32bdcb4ddd9307482b468cdd371493197b6628f0
Laura <law7676@outlook.com>
2021-06-05 10:52:56 -0500

    add second string interpolation solution

---
+++ 02.08-string-interpolation-part-2.dart
@@ -0,0 +1,8 @@
+void main() {
+  double temperature = 20;
+  int value = 2;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+  
+  print('The temperature is ${temperature}.\n${value} plus ${value} equals ${2*value}.\nI like gluten-free ${pizza} and ${pasta}.');
+}
