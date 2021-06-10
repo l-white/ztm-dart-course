d714f070692ebb5a66f48bb13d8f3438d4248e39
Laura <law7676@outlook.com>
2021-06-10 17:29:18 -0500

    Added switch statement solution

---
+++ solutions/04.08-switch.dart
@@ -0,0 +1,16 @@
+void main(){
+  const pos = 0;
+  switch (pos) {
+    case 1:
+      print('gold');
+      break;
+    case 2:
+      print('silver');
+      break;
+    case 3:
+      print('bronze');
+      break;
+    default:
+      print('no medal');
+  }
+}
